37b3de0949d21eca2098364c7306d2d1bc8f07fb
Px3 <px3@protonmail.com>
2016-10-05 12:43:53 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,19 +7,17 @@
 	echo -e "Do you want to install VM Tools ? (Y/N)"
 			read install
 			if [[ $install = Y || $install = y ]] ; then	
-				echo -e "\033[31m====== Adding Open VM Tools To the List ======\033[m"
-				sleep 2
+				echo -e " Adding Open VM Tools To the List ======"
 				echo 'sudo apt-get -y install open-vm-tools-desktop fuse \' >> aptstall.sh
 			else
 				echo -e "\e[32m[-] Ok,!\e[0m"
 			fi
 #install Google Chrome 
 	echo -e "\e[1;31mThis option will install Google Chrome!\e[0m"
-	echo -e ""
 	echo -e "Do you want to install it ? (Y/N)"
 			read install
 			if [[ $install = Y || $install = y ]] ; then	
-				echo -e "\033[31m====== Adding Google Chrome To the List ======\033[m"
+				echo -e "====== Adding Google Chrome To the List ======"
 				sleep 2
 				echo 'sudo apt-get -y install open-vm-tools-desktop fuse \' >> aptstall.sh
 				echo 'sudo add-apt-repository -y "deb http://dl.google.com/linux/chrome/deb/ stable main" '>> repos.sh
@@ -27,21 +25,16 @@
 				echo -e "\e[32m[-] Ok,maybe later !\e[0m"
 			fi
 #install
-	echo -e "\e[1;31mThis option will Add All other Kali Linux Repos!\e[0m"
-	echo -e ""
-	echo -e "Do you want to install it ? (Y/N)"
+echo "_____________________________________________________"
+	echo -e "Install Scapy"
 			read install
 			if [[ $install = Y || $install = y ]] ; then	
-				echo -e "\033[31m====== Adding The Rest Of Kali Linus To the List ======\033[m"
-				sleep 2
-				rm /etc/apt/sources.list
-				echo "" >> /etc/apt/sources.list
-				echo 'deb http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list
-				echo 'deb-src http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list
-				echo 'deb http://old.kali.org/kali sana main non-free contrib' >> /etc/apt/sources.list
-				apt-get update
+				echo -e "Adding Scapy to the list"
+				echo 'sudo apt-get -y install scapy \' >> aptstall.sh
+				echo "_____________________________________________________"
 			else
-				echo -e "\e[32m[-] Ok, Next \e[0m"
+				echo -e "Ok"
+				echo "_____________________________________________________"
 			fi
 
 ##### UPDATE AND UPGRADE EVERYTHING IN KALI #####
