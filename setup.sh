00618fff41c4d38a7a5f8d2b5e4ed69cceca65f5
Px3 <px3@protonmail.com>
2016-10-05 15:46:22 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,61 +1,218 @@
 #!/bin/bash
 
-
+	echo ""
+	echo " ================================================================================= "
+	echo "| Selecting Applications                                                          |"
+	echo "|     Selecting Repositories                                                      |"
+	echo "|     Selecting Setting And Tweaks                                                |"
+	echo "|     Getting Updates                                                             |"
+	echo "|     Adding Selected Repositories                                                |"
+	echo "|     Adding Applications with Git                                                |"
+	echo "|     Adding Applicaitons with Apt-Get                                            |"
+	echo "|     Adding Tweaks and Settings                                                  |"
+	echo "|     Cleaning Up The Mess                                                        |"
+	echo " ================================================================================= "
+	echo ""
 
 # Install VM tools from Kali repo  
-	echo -e "\e[1;31mFOR VMWARE BUILDS ONLY!\e[0m"
-	echo -e "Do you want to install VM Tools ? (Y/N)"
-			read install
-			if [[ $install = Y || $install = y ]] ; then	
-				echo -e " Adding Open VM Tools To the List ======"
-				echo 'sudo apt-get -y install open-vm-tools-desktop fuse \' >> aptstall.sh
-			else
-				echo -e "\e[32m[-] Ok,!\e[0m"
-			fi
-#install Google Chrome 
-	echo -e "\e[1;31mThis option will install Google Chrome!\e[0m"
-	echo -e "Do you want to install it ? (Y/N)"
-			read install
-			if [[ $install = Y || $install = y ]] ; then	
-				echo -e "====== Adding Google Chrome To the List ======"
-				sleep 2
-				echo 'sudo apt-get -y install open-vm-tools-desktop fuse \' >> aptstall.sh
-				echo 'sudo add-apt-repository -y "deb http://dl.google.com/linux/chrome/deb/ stable main" '>> repos.sh
-			else
-				echo -e "\e[32m[-] Ok,maybe later !\e[0m"
-			fi
-#install
-echo "_____________________________________________________"
-	echo -e "Install Scapy? (Y/n)"
-			read install
-			if [[ $install = Y || $install = y ]] ; then	
-				echo -e "Adding Scapy to the list"
-				echo 'sudo apt-get -y install scapy \' >> aptstall.sh
-				echo "_____________________________________________________"
-			else
-				echo -e "Ok"
-				echo "_____________________________________________________"
-			fi
+	echo "_____________________________________________________"
+	echo ""
+	echo -e " [ INSTALL VMWARE TOOLS? (Y/N) ]"
+	read install
+	if [[ $install = Y || $install = y ]] ; then	
+		echo "+++ Adding VMware Tools To the List +++"
+		echo 'sudo apt-get -y install open-vm-tools-desktop fuse \' >> aptstall.sh
+	else
+		echo "--- Not Added --- "
+	fi
+#GOOGLE CHROME INSTALL 
+	echo "_____________________________________________________"
+	echo ""
+	echo -e "[ INSTALL GOOGLE CHROME? (Y/N) ]"
+	read install
+	if [[ $install = Y || $install = y ]] ; then	
+		echo "+++ Adding Chrome To The List +++"
+		echo 'sudo apt-get -y install open-vm-tools-desktop fuse \' >> aptstall.sh
+		echo 'sudo add-apt-repository -y "deb http://dl.google.com/linux/chrome/deb/ stable main" '>> repos.sh
+	else
+		echo "--- Not Added ---"
+	fi
+#SCAPY INSTALL
+	echo "_____________________________________________________"
+	echo ""
+	echo "[ INSTALL SCAPY? (Y/n)] "
+	read install
+	if [[ $install = Y || $install = y ]] ; then	
+		echo "+++ Adding Scapy to the list +++"
+		echo 'sudo apt-get -y install scapy \' >> aptstall.sh
+	else
+		echo "--- Not Added ---"
+	fi
+#EMPIRE INSTALL
+	echo "_____________________________________________________"
+	echo ""
+	echo "[ INSTALL EMPIRE? (Y/n)] "
+	read install
+	if [[ $install = Y || $install = y ]] ; then	
+		echo "+++ Adding Empire to the list +++"
+		echo 'https://github.com/adaptivethreat/Empire.git' >> gitstall.sh
+	else
+		echo "--- Not Added ---"
+	fi
+#SET INSTALL 
+	echo "_____________________________________________________"
+	echo ""
+	echo "[ INSTALL S.E.T? (Y/n)] "
+	read install
+	if [[ $install = Y || $install = y ]] ; then	
+		echo "+++ Adding S.E.T. to the list +++"
+		echo 'https://github.com/trustedsec/social-engineer-toolkit.git' >> gitstall.sh
+	else
+		echo "--- Not Added ---"
+	fi
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+	echo ""
+	echo " ================================================================================= "
+	echo "|     Selecting Applications                                                      |"
+	echo "| Selecting Repositories                                                          |"
+	echo "|     Selecting Setting And Tweaks                                                |"
+	echo "|     Getting Updates                                                             |"
+	echo "|     Adding Selected Repositories                                                |"
+	echo "|     Adding Applications with Git                                                |"
+	echo "|     Adding Applicaitons with Apt-Get                                            |"
+	echo "|     Adding Tweaks and Settings                                                  |"
+	echo "|     Cleaning Up The Mess                                                        |"
+	echo " ================================================================================= "
+	echo ""
+
+#TWEAKS TO USE 
+	echo ""
+	echo " ================================================================================= "
+	echo "|     Selecting Applications                                                      |"
+	echo "|     Selecting Repositories                                                      |"
+	echo "| Selecting Setting And Tweaks                                                    |"
+	echo "|     Getting Updates                                                             |"
+	echo "|     Adding Selected Repositories                                                |"
+	echo "|     Adding Applications with Git                                                |"
+	echo "|     Adding Applicaitons with Apt-Get                                            |"
+	echo "|     Adding Tweaks and Settings                                                  |"
+	echo "|     Cleaning Up The Mess                                                        |"
+	echo " ================================================================================= "
+	echo ""
+
 
 ##### UPDATE AND UPGRADE EVERYTHING IN KALI #####
-echo "<><><><><> Lets Get All Up To Dated.	<><><><><>" 
+	echo ""
+	echo " ================================================================================= "
+	echo "|     Selecting Applications                                                      |"
+	echo "|     Selecting Repositories                                                      |"
+	echo "|     Selecting Setting And Tweaks                                                |"
+	echo "| Getting Updates                                                                 |"
+	echo "|     Adding Selected Repositories                                                |"
+	echo "|     Adding Applications with Git                                                |"
+	echo "|     Adding Applicaitons with Apt-Get                                            |"
+	echo "|     Adding Tweaks and Settings                                                  |"
+	echo "|     Cleaning Up The Mess                                                        |"
+	echo " ================================================================================= "
+	echo ""
 #sudo apt-get -y --force-yes update
 #sudo apt-get -y --force-yes upgrade
-echo "<><><><><> Your Are All Up To Date.	<><><><><>"
+	echo "<><><><><> All Updates Complete.          <><><><><>" 
+
 #####Run Install Scripts#####
+
 #install needed repos 
-echo "<><><><><> Adding Repos.   		<><><><><>"
+	echo ""
+	echo " ================================================================================= "
+	echo "|     Selecting Applications                                                      |"
+	echo "|     Selecting Repositories                                                      |"
+	echo "|     Selecting Setting And Tweaks                                                |"
+	echo "|     Getting Updates                                                             |"
+	echo "| Adding Selected Repositories                                                    |"
+	echo "|     Adding Applications with Git                                                |"
+	echo "|     Adding Applicaitons with Apt-Get                                            |"
+	echo "|     Adding Tweaks and Settings                                                  |"
+	echo "|     Cleaning Up The Mess                                                        |"
+	echo " ================================================================================= "
+	echo ""
 sudo sh ./repos.sh
-echo "<><><><><> Repos Added, Next Step... 	<><><><><>"
+	echo "<><><><><> Selected Repos Added           <><><><><>"
+
 #install from GIT
-echo "<><><><><> Running Script For Github apps	<><><><><>"
+	echo ""
+	echo " ================================================================================= "
+	echo "|     Selecting Applications                                                      |"
+	echo "|     Selecting Repositories                                                      |"
+	echo "|     Selecting Setting And Tweaks                                                |"
+	echo "|     Getting Updates                                                             |"
+	echo "|     Adding Selected Repositories                                                |"
+	echo "| Adding Applications with Git                                                    |"
+	echo "|     Adding Applicaitons with Apt-Get                                            |"
+	echo "|     Adding Tweaks and Settings                                                  |"
+	echo "|     Cleaning Up The Mess                                                        |"
+	echo " ================================================================================= "
+	echo ""
 sudo sh ./gitstall.sh 
-echo "<><><><><> Finished With The Github Apps. <><><><><>"
+	echo "<><><><><> Finished With The Github Apps. <><><><><>"
+
 #Install from apt-get
-echo "<><><><><> Installing The Other Apps	<><><><><>"
+	echo ""
+	echo " ================================================================================= "
+	echo "|     Selecting Applications                                                      |"
+	echo "|     Selecting Repositories                                                      |"
+	echo "|     Selecting Setting And Tweaks                                                |"
+	echo "|     Getting Updates                                                             |"
+	echo "|     Adding Selected Repositories                                                |"
+	echo "|     Adding Applications with Git                                                |"
+	echo "| Adding Applicaitons with Apt-Get                                                |"
+	echo "|     Adding Tweaks and Settings                                                  |"
+	echo "|     Cleaning Up The Mess                                                        |"
+	echo " ================================================================================= "
+	echo ""
 sudo sh ./aptstall.sh 
-echo "<><><><><> Done With All Apps. 		<><><><><>" 
+	echo "<><><><><> Done With All Apps.            <><><><><>" 
+
+#Adding Additional Settings
+	echo ""
+	echo " ================================================================================= "
+	echo "|     Selecting Applications                                                      |"
+	echo "|     Selecting Repositories                                                      |"
+	echo "|     Selecting Setting And Tweaks                                                |"
+	echo "|     Getting Updates                                                             |"
+	echo "|     Adding Selected Repositories                                                |"
+	echo "|     Adding Applications with Git                                                |"
+	echo "|     Adding Applicaitons with Apt-Get                                            |"
+	echo "| Adding Tweaks and Settings                                                      |"
+	echo "|     Cleaning Up The Mess                                                        |"
+	echo " ================================================================================= "
+	echo ""
+
 #clean Things up
-echo "<><><><><> Cleaning Up The Unneeded Mess 	<><><><><>"
+	echo ""
+	echo " ================================================================================= "
+	echo "|     Selecting Applications                                                      |"
+	echo "|     Selecting Repositories                                                      |"
+	echo "|     Selecting Setting And Tweaks                                                |"
+	echo "|     Getting Updates                                                             |"
+	echo "|     Adding Selected Repositories                                                |"
+	echo "|     Adding Applications with Git                                                |"
+	echo "|     Adding Applicaitons with Apt-Get                                            |"
+	echo "|     Adding Tweaks and Settings                                                  |"
+	echo "| Cleaning Up The Mess                                                            |"
+	echo " ================================================================================= "
+	echo ""
 sudo apt autoremove
-echo " <><><><><> You Are All Ready To Go. 	<><><><><>"
+	echo "<><><><><> You Are All Ready To Go.       <><><><><>"
