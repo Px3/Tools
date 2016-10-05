154226ecb8b46243a27bc4dd1bf4c6b1bbd1cdf1
Px3 <px3@protonmail.com>
2016-10-05 12:23:28 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,30 +3,16 @@
 
 ##### ADD NEW REPOS #####
 
-#Google Chrome 
-sudo add-apt-repository -y "deb http://dl.google.com/linux/chrome/deb/ stable main"
-
-#
-
-
-#
-
-
-#
-
-
-#
-
 
 ##### UPDATE AND UPGRADE #####
 
-sudo apt-get -y --force-yes update
-sudo apt-get -y --force-yes upgrade
+#sudo apt-get -y --force-yes update
+#sudo apt-get -y --force-yes upgrade
 
 ##### INSTALL APPS FROM REPOS
 
-sudo apt-get -y install \
-	git gitk gitg google-chrome-stable curl libcurl3 scapy 
+#sudo apt-get -y install \
+#	git gitk gitg curl libcurl3 scapy 
 	
 	
 ##### INSTALL INDIVIDUAL APPS #####
@@ -37,7 +23,6 @@ sudo apt-get -y install \
 ##### CHOOSE ADDITIONAL APPS TO INSTALL #####
 
 # Install VM tools from Kali repo  
-function installvmtools {
 	echo -e "\e[1;31mThis option will install VMWare Tools!\e[0m"
 	echo -e "\e[1;31mFOR VMWARE BUILDS ONLY!\e[0m"
 	echo -e ""
@@ -50,83 +35,59 @@ function installvmtools {
 			else
 				echo -e "\e[32m[-] Ok,maybe later !\e[0m"
 			fi
-}
-
-#install 
-
-
+#install Google Chrome 
+	echo -e "\e[1;31mThis option will install Google Chrome!\e[0m"
+	echo -e ""
+	echo -e "Do you want to install it ? (Y/N)"
+			read install
+			if [[ $install = Y || $install = y ]] ; then	
+				echo -e "\033[31m====== Adding Google Chrome To the List ======\033[m"
+				sleep 2
+				echo 'sudo apt-get -y install open-vm-tools-desktop fuse \' >> aptstall.sh
+				echo 'sudo add-apt-repository -y "deb http://dl.google.com/linux/chrome/deb/ stable main" '>> repos.sh
+			else
+				echo -e "\e[32m[-] Ok,maybe later !\e[0m"
+			fi
 #install
+	echo -e "\e[1;31mThis option will Add All other Kali Linux Repos!\e[0m"
+	echo -e ""
+	echo -e "Do you want to install it ? (Y/N)"
+			read install
+			if [[ $install = Y || $install = y ]] ; then	
+				echo -e "\033[31m====== Adding The Rest Of Kali Linus To the List ======\033[m"
+				sleep 2
+				rm /etc/apt/sources.list
+				echo "" >> /etc/apt/sources.list
+				echo 'deb http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list
+				echo 'deb-src http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list
+				echo 'deb http://old.kali.org/kali sana main non-free contrib' >> /etc/apt/sources.list
+				apt-get update
+			else
+				echo -e "\e[32m[-] Ok, Next \e[0m"
+			fi
 
-
-#Install 
 ##### UPDATE AND UPGRADE EVERYTHING IN KALI #####
-function updatekali {
-clear
-echo -e "
-\033[31m#######################################################\033[m
-                Let's Update Kali
-\033[31m#######################################################\033[m"
-select menusel in "Update sources.list (Included kali sana repository for installing more package)" "Update Kali Sana 2.0 to Kali 2016.2" "Update and Clean Kali" "Back to Main"; do
-case $menusel in
-        "Update sources.list (Included kali sana repository for installing more package)")
-                
-		echo -e "\033[31m====== Adding new sources list and updating ======\033[m"
-		rm /etc/apt/sources.list
-		echo "" >> /etc/apt/sources.list
-		echo 'deb http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list
-		echo 'deb-src http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list
-		echo 'deb http://old.kali.org/kali sana main non-free contrib' >> /etc/apt/sources.list
-		apt-get update
-		pause
-		clear ;;	
-	"Update Kali Sana 2.0 to Kali 2016.2")
-		clear
-		echo -e "\033[32mUpdating Kali Sana to Kali Linux 2016.2\033[m"
-		rm /etc/apt/sources.list
-		echo "" >> /etc/apt/sources.list
-		echo 'deb http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list
-		echo 'deb-src http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list
-		#apt-get update && apt-get -y dist-upgrade
-		apt-get update && apt-get -y upgrade 
-		echo -e "\033[32mDone updating kali. You need to reboot your Kali Linux system\033[m"
-		pause
-		clear ;;
-	
-	"Update and Clean Kali")
-		clear
-		echo -e "\033[32mUpdating and Cleaning Kali\033[m"
-		apt-get update && apt-get -y dist-upgrade && apt-get autoremove -y && apt-get -y autoclean
-		echo -e "\033[32mDone updating and cleaning kali\033[m" ;;
-		
-	"Back to Main")
-		clear
-		mainmenu ;;
-		
-	*)
-		screwup
-		updatekali ;;
-
-esac
-
-break
-
-done
-}
-
-
+echo "<><><><><> First, Lets Get All Up To Date. <><><><><>" 
+sudo apt-get -y --force-yes update
+sudo apt-get -y --force-yes upgrade
+echo "<><><><><> Your Are All Up To Date. <><><><><>"
 #####Run Install Scripts#####
-
 #install needed repos 
-echo "Adding Needed Repos"
-sh ./repos
-echo "Repos Added, Now On tTo Next Step..."
+echo "<><><><><> Adding Repos. <><><><><>"
+sudo sh ./repos.sh
+echo "<><><><><> Repos Added, Now On To The Next Step... <><><><><>"
 
 #install from GIT
-echo "Running Script For Github apps."
-sh ./gitstall.sh 
-echo "Finished With Apps From Github"
+echo "<><><><><> Running Script For Github apps. <><><><><>"
+sudo sh ./gitstall.sh 
+echo "<><><><><> Finished With Just The Github Apps. <><><><><>"
 
 #Install from apt-get
-echo "Now Installing All Other Apps"
-sh ./aptstall 
-echo "Done With All Other Apps" 
+echo "<><><><><> Now Installing The Other Apps With apt-get <><><><><>"
+sudo sh ./aptstall 
+echo "<><><><><> Done With All Apps. <><><><><>" 
+
+#clean Things up
+echo "<><><><><> Cleaning Up The Unneeded Mess <><><><><>"
+sudo apt autoremove
+echo " <><><><><> You Are All Ready To Go. <><><><><>
